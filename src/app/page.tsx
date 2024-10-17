a4699fb5675ae80251916919628db82648ef470b
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-10-17 17:34:50 +0530

    Add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
-    <h1>Welcome to e-lib</h1>
+    <>
+      <Banner />
+    </>
   );
 }
